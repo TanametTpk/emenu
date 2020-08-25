f769608d160e6b5882e8fa703d08d827202f5c04
Tui Tpk <tanamet.tpk@gmail.com>
2020-08-25 16:54:04 +0700

    Updates

---
+++ precache-manifest.87699c903c5978f5c7bdfe7f6926d659.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e073ac46a7dec89e0114e932f66d04aa",
+    "url": "/emenu/index.html"
+  },
+  {
+    "revision": "823afa67f2488ff1394d",
+    "url": "/emenu/static/css/main.be0bdc9b.chunk.css"
+  },
+  {
+    "revision": "d499192be7504bc23e8c",
+    "url": "/emenu/static/js/2.7b565ce8.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/emenu/static/js/2.7b565ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "823afa67f2488ff1394d",
+    "url": "/emenu/static/js/main.16e1addd.chunk.js"
+  },
+  {
+    "revision": "5ebfce14f752c06631f7",
+    "url": "/emenu/static/js/runtime-main.9ff76f86.js"
+  },
+  {
+    "revision": "03ad602c8797a30f1415c173e5940e3b",
+    "url": "/emenu/static/media/photoEmpty.03ad602c.svg"
+  }
+]);
